ae8f6a603fb06f5f880635de44bdea96ac2b49fa
KinashiRintaro <kinashi_r@mediaxis.jp>
2022-10-26 16:53:13 +0900

    groupとdatasetの概念は理解した

--- opt/makeHdf5.py
+++ opt/makeHdf5.py
@@ -1,6 +1,22 @@
 import h5py
  
-with h5py.File("test.hdf5", "w") as f:
-    dset = f.create_dataset("test", (2, 2))
+with h5py.File("mdmp-app.hdf5", "w") as f:
+    # 読み込んだCSVの形状を取得
+    matrix = (4, 2)
+
+    # 以下の形状でグループ（フォルダ）を作成できる
+    g_monipad=f.create_group('monipad')
+
+    # データセット（ファイルに当たるもの）を作成
+    dset = f.create_dataset("monipad/monipad_active_energy_burneds", matrix)
     dset[0, 0] = 3.0
     dset[0, 1] = 10.0
+
+    dset = f.create_dataset("monipad/monipad_dyskinetic_symptoms", matrix)
+    dset[0, 0] = 5.0
+    dset[0, 1] = 12.0
+
+    g_user=f.create_group('user')
+    dset = f.create_dataset("user/personal_users", matrix)
+    dset[0, 0] = 103.0
+    dset[0, 1] = 104.0
--- opt/readHdf5.py
+++ opt/readHdf5.py
@@ -1,7 +1,9 @@
 import h5py
  
-with h5py.File("test.hdf5", "r") as f:
-    data_1 = f["test"][0, 0]
-    data_2 = f["test"][0, 1]
+with h5py.File("mdmp-app.hdf5", "r") as f:
+    data_1 = f["monipad/monipad_active_energy_burneds"][0, 0]
+    data_2 = f["monipad/monipad_dyskinetic_symptoms"][0, 1]
+    data_3 = f["user/personal_users"][0, 1]
     print(data_1)
     print(data_2)
+    print(data_3)
